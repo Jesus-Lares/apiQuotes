d87e7e4fc47e680ffb73e140f3ea9c2dbed36d3e
Jesus Lares Contreras <jesus.lares2000@gmail.com>
2022-04-08 19:45:44 -0500

    Se agrego la configuración de las cors

--- src/app.ts
+++ src/app.ts
@@ -22,6 +22,17 @@ function init() {
     res.status(200).send("API QUOTES");
   });
 
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+  });
+
   app.listen(port, () => {
     console.log("##############");
     console.log("###API REST###");
